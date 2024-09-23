1a7c1cc02739ad02135446234f4eed7a4df135b2
Asanka Wickramasurendra <asankadilshanme@gmail.com>
2024-09-23 16:26:41 +0530

    feat:added route

--- Frontend/src/Pages/DashBoard.jsx
+++ Frontend/src/Pages/DashBoard.jsx
@@ -3,7 +3,7 @@ import { useLocation } from "react-router-dom";
 import DashSideBar from "../Components/DashSideBar";
 import DashProfile from "../Components/DashProfile";
 import DashUsers from "../Components/DashUsers";
-
+import Dashstaff from "../Components/Dashstaff";
 import DashRequests from "../Components/DashRequests";
 import DashMyRequests from "../Components/DashMyRequests";
 import DashProduct from "../Components/DashProduct";
@@ -31,7 +31,8 @@ export default function DashBoard() {
       </div>
       {tab==='profile' && <DashProfile/>}
       {tab === 'users' && <DashUsers/>}
-      {tab === 'staff' && <Dashstaff/>} {tab === 'requests' && <DashRequests/>}
+      {tab === 'staff' && <Dashstaff/>}
+      {tab === 'requests' && <DashRequests/>}
       {tab === 'my_requests' && <DashMyRequests/>}
       {tab === 'products' && <DashProduct/>}
       {tab === 'my_orders' && <DashMyOrders/>}
